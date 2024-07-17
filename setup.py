9bde13d49e69f2b6d6670a37f229cb3896bbb028
Karlo Krakan <69814671+karlokr-p@users.noreply.github.com>
2024-07-17 13:44:15 -0600

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="notificationhubs-rest-python",
-    version="1.0.0",
+    version="1.0.1",
     include_package_data=True,
     license="Apache-2.0 License",
     description="A Python library for sending notifications using Azure Notification Hubs REST API.",
